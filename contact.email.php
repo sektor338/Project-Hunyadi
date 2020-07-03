847289604d6916ea6dbc6ec24044ff5623df9702
Martin Mészáros <hdsektor38@gmail.com>
2020-07-03 22:39:27 +0200

    Contact page downgrade

--- contact.email.php
+++ contact.email.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 $to = "hdsektor38@gmail.com";
 $subject = $_POST['c-subject'] . PHP_EOL;
 $message = "Name: " . $_POST['c-name'] . PHP_EOL;
@@ -10,5 +10,4 @@ if (@mail($to, $subject, $message)) {
   header("Location: index.php");
 } else {
   header("Location: contacterror.php");
-}
-?>
+}*/
--- contact.php
+++ contact.php
@@ -2,47 +2,21 @@
 <html lang="en">
       <head>
             <?php include 'header.php';?>
-            <script src="main.js"></script>
-            <link rel="stylesheet" href="index.css">
+          <link rel="stylesheet" href="contact.css">
       </head>
       <body>
             <?php include 'navbar.php'; ?>
             <main>
-
-                  <center>
-                        <div class="contactdiv">
-                              <h1>CONTACT US</h1>
-                              <form class="" action="contact.email.php" method="POST">
-                                    <label for="c-name">Name</label><br>
-                                    <input type="text" name="c-name" minlength="4" maxlength="255">
-                                    <br>
-                                    <br>
-                                    <label for="c-email">Email</label><br>
-                                    <input type="email" name="c-email" minlength="5" maxlength="255">
-                                    <br>
-                                    <br>
-                                    <label for="c-subject">Subject</label><br>
-                                    <select class="subject" name="c-subject">
-                                          <option value="problem">Problem</option>
-                                          <option value="idea">Idea</option>
-                                    </select>
-                                    <br>
-                                    <br>
-                                    <label for="c-text">Message</label><br>
-                                    <textarea name="c-text" minlength="10"></textarea>
-                                    <br>
-                                    <br>
-                                    <input type="submit" value="Submit">
-                              </form>
-                        </div>
-                  </center>
-
-
+                <div id="contactdiv">
+                    <h1>Attention!</h1>
+                    <p id="contactp">This page is no longer supported and it will be deleted. <br>
+                        In order to report a problem/bug or suggest an idea please go the site's Github page and submit a new issue.
+                        <br>
+                        Link is in the footer section under Development section.
+                    </p>
+                </div>
             </main>
 
             <?php include 'footer.php'; ?>
-
-                  <?php // IDEA: chat, jobb alul ?>
-
       </body>
 </html>
