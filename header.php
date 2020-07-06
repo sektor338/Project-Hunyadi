4badb4826a223e0dc7e413e27cda6919f3509e77
Martin Mészáros <hdsektor38@gmail.com>
2020-07-06 14:04:35 +0200

    Things and stuff
    Took 10 minutes

--- header.php
+++ header.php
@@ -4,7 +4,13 @@ include_once 'dbh.inc.php';
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
-<link rel="icon" href="pictures/logo/Schwarze Sonne.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="theme-color" content="#ffffff">
 <meta name="description" content="The best place on the internet">
 <meta name="keywords" content="national socialism, Hitler, funny">
 <meta name="author" content="Martin Mészáros">
